15ccfabe298a9a1efd7224abe2bc202b85a9b365
Risav Sarkar <risavabir2001@gmail.com>
2021-11-11 13:22:33 +0530

    Initial Commit

---
+++ client/src/App.js
@@ -0,0 +1,8 @@
+import PageLayout from "./components/pageLayout";
+import "./styles/styles.css";
+
+const App = () => {
+  return <PageLayout />;
+};
+
+export default App;
---
+++ client/src/components/pageLayout.js
@@ -0,0 +1,57 @@
+import ProfilePic from "../assets/Profile.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHome,
+  faEnvelope,
+  faBookmark,
+  faUserCircle,
+  faCog,
+  faSignOutAlt,
+} from "@fortawesome/free-solid-svg-icons";
+
+const PageLayout = () => {
+  return (
+    <div className="layout">
+      <div className="navBar">
+        <div className="navHeader">
+          <img src={ProfilePic}></img>
+          <p>Risav Sarkar</p>
+        </div>
+
+        <div className="navButtons">
+          <button>
+            <FontAwesomeIcon icon={faHome} />
+            <p>Home</p>
+          </button>
+          <button>
+            <FontAwesomeIcon icon={faEnvelope} />
+            <p>Messages</p>
+          </button>
+          <button>
+            <FontAwesomeIcon icon={faUserCircle} />
+            <p>Profile</p>
+          </button>
+          <button>
+            <FontAwesomeIcon icon={faBookmark} />
+            <p>Saved</p>
+          </button>
+          <button>
+            <FontAwesomeIcon icon={faCog} />
+            <p>Settings</p>
+          </button>
+        </div>
+
+        <div className="navFooter">
+          <img src={ProfilePic}></img>
+          <h1>Devcord</h1>
+          <button>
+            <FontAwesomeIcon icon={faSignOutAlt} />
+          </button>
+        </div>
+      </div>
+      <div className="content"></div>
+    </div>
+  );
+};
+
+export default PageLayout;
